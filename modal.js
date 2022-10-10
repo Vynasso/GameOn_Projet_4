7e54b2bfbfc4a02689c60bd759aae741ab688689
Vynasso <vynasso.doffonou@gmail.com>
2022-10-10 11:50:52 +0200

    integration of regexes, progress on checking the validity of the form

--- modal.js
+++ modal.js
@@ -13,6 +13,67 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
 
+//Form Control
+const firstName = document.getElementById("firstName")
+const lastName = document.getElementById("lastName")
+const email = document.getElementById("email")
+const birthdate = document.getElementById("birthdate")
+
+// Regex
+let regexEmail = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/
+let regexFirstAndLastName = /^[a-z ,.'-]+$/i
+let regexBirthdate = /^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/
+
+// Checking Function
+function isFirstNameValid(){
+  if(firstName.value.length < 2){
+    return false
+  } else if (firstName.value.length > 2 && regexFirstAndLastName.test(firstName.value)){
+    return true
+  }
+}
+
+function isLastNameValid(){
+  if(lastName.value.length < 2){
+    return false
+  } else if (lastName.value.length > 2 && regexFirstAndLastName.test(lastName.value)){
+    return true
+  }
+}
+
+function isBirthdateValid(){
+  if(birthdate.value.length < 9){
+    return false
+  } else if (birthdate.value.length > 9 && regexBirthdate.test(birthdate.value)){
+    return true
+  }
+}
+
+function isEmailValid(){
+  if(regexEmail.test(email.value)){
+    return true
+  }
+}
+
+function isFormValid(){
+  if(isFirstNameValid() && isLastNameValid()){
+    return true
+  }
+}
+
+document.addEventListener("submit", function (event){
+  console.log("ok")
+  if(isFormValid()){
+    console.log("working")
+  }
+})
+
+
+// Submit Button
+const btnSubmit = document.getElementById("submit")
+
+// btnSubmit.addEventListener('click',isEmail)
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -25,6 +86,6 @@ closeBtn.forEach( (btn) => btn.addEventListener("click", closeModal));
 
 // close modal form
 function closeModal(){
-  console.log("working ?")
   modalbg.style.display = "none";
+
 }
